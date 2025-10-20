566519dfe9d703f64325950c6b1d4786821a5ceb
Aleksandra Vojinović <aleksandra@gmail.com>
2025-10-20 15:42:42 +0200

    Prvi commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="sr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja ePijaca</title>
+</head>
+<body>
+    <h1>Dobrodošli na ePijacu</h1>
+     <nav>
+        <a href="index.php" class="nav-link">Početna</a>
+        <a href="galerija.php" class="nav-link">Galerija</a>
+        <a href="kontakt.php" class="nav-link">Kontakt</a>
+    </nav>
+    <button class="btn">Pogledaj proizvode</button>
+</body>
+</html>
