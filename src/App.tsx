9c6c871a8accb5a6189063931c249ece22209a1a
Strahinja Babic <strajaba@gmail.com>
2025-10-21 19:14:01 +0200

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,18 @@ import Products from './pages/Products';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
-import Layout from './components/Layout';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/products' element={<Products />} />
-            <Route path='/products/:id' element={<Product />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/checkout' element={<Checkout />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/products/:id' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
